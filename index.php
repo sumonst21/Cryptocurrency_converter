9c40c0faa7510ba746ff33ee848280462af52795
jens210 <jensvk94@gmail.com>
2018-01-02 13:50:08 +0100

    dropdown added
    the chosen fiat currency get converted to Bitcoin

--- index.php
+++ index.php
@@ -1,23 +1,78 @@
 <!DOCTYPE html>
-<?php 
-$tick = file_get_contents('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=DKK');
-$url = $tick;
-$data = json_decode($tick, TRUE);
-$cur = $data[0]["price_dkk"];
-echo $cur;
-$curDisplay = round($cur, 2);
-?>
 <html>
 <body>
+
+ <h1>Coinvert!</h1>
+<div id="btc"></div>
+<div id="container">
+
+<input type="text" id="bi" onchange="CryptoConvert(this);" onkeyup="CryptoConvert(this);"/> 
+<input type="text" name="ci" id="ci"onchange="FiatConvert(this);" onkeyup="FiatConvert(this);"/> <br>
+
+<br>
+<form action="#" method="post">
+<select name="Fiat">
+<option value="USD">USD</option>
+<option value="EUR">EUR</option>
+<option value="SEK">SEK</option>
+<option value="NOK">NOK</option>
+<option value="DKK">DKK</option>
+</select>
+<input type="submit" name="submit" value="Get Selected Values" />
+</form>
+<?php
+
+
+if(isset($_POST['submit'])){
+$selected_val = $_POST['Fiat'];  // Storing Selected Value In Variable
+echo "You have selected :" .$selected_val;  // Displaying Selected Value
+}
+if($_POST['Fiat'] === 'EUR'){
+$tick = file_get_contents('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR'); 
+$url = $tick;
+$data = json_decode($tick, TRUE);
+$cur = $data[0]["price_eur"]; //price_fiat
+$curDisplay = round($cur, 2);
+}
+else if($_POST['Fiat'] === 'DKK'){
+    $tick = file_get_contents('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=DKK'); 
+    $url = $tick;
+    $data = json_decode($tick, TRUE);
+    $cur = $data[0]["price_dkk"]; //price_fiat
+    $curDisplay = round($cur, 2);
+    }
+    else if($_POST['Fiat'] === 'NOK'){
+        $tick = file_get_contents('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=NOK'); 
+        $url = $tick;
+        $data = json_decode($tick, TRUE);
+        $cur = $data[0]["price_nok"]; //price_fiat
+        $curDisplay = round($cur, 2);
+        }
+        else if($_POST['Fiat'] === 'SEK'){
+            $tick = file_get_contents('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=SEK'); 
+            $url = $tick;
+            $data = json_decode($tick, TRUE);
+            $cur = $data[0]["price_sek"]; //price_fiat
+            $curDisplay = round($cur, 2);
+            }
+else{
+    $tick = file_get_contents('https://api.coinmarketcap.com/v1/ticker/bitcoin/'); 
+    $url = $tick;
+    $data = json_decode($tick, TRUE);
+    $cur = $data[0]["price_usd"]; //price_fiat
+    $curDisplay = round($cur, 2);  
+}
+
+?>
     <script>
-    function btcConvert(input){
+    function CryptoConvert(input){
         var price = "<?php echo $curDisplay; ?>"; 
         var output = input.value * price;
         var co = document.getElementById('ci');
         ci.value = output;
     }
 
-    function curConvert(input){
+    function FiatConvert(input){
         var price = "<?php echo $curDisplay; ?>"; 
         var output = input.value / price;
         var co = document.getElementById('bi');
@@ -25,15 +80,6 @@ $curDisplay = round($cur, 2);
     }
 
     </script>
-     <h1>Coinvert!</h1>
-<div id="btc"></div>
-<div id="container">
-
-<input type="text" id="bi" onchange="btcConvert(this);" onkeyup="btcConvert(this);"/> BTC <=>
-<input type="text" name="ci" id="ci"onchange="curConvert(this);" onkeyup="curConvert(this);"/> DKK<br>
-
-
-
 </div>
 
 </body>
