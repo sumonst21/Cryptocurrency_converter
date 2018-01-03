37db58359eac81316603839003da1f6d6d3256e2
jens210 <jensvk94@gmail.com>
2018-01-03 13:26:26 +0100

    default state added for the converter

--- index.php
+++ index.php
@@ -27,19 +27,25 @@
 
 <?php
 if(isset($_POST['submit'])){
-$selected_crypto = $_POST['Crypto'];  // Storing Selected Value In Variable
-echo " " .$selected_crypto;  // Displaying Selected Value
-
-$selected_fiat = $_POST['Fiat'];  // Storing Selected Value In Variable
-echo " " .$selected_fiat;  // Displaying Selected Value
+$selected_crypto = $_POST['Crypto'];  // Storing Selected Crypto Value In Variable
+echo " " .$selected_crypto;  // Displaying Selected Crypto Value
+$selected_fiat = $_POST['Fiat'];  // Storing Selected Fiat Value In Variable
+echo " " .$selected_fiat;  // Displaying Selected Fiat Value
 }
-
+if($_POST['Fiat'] && $_POST['Crypto'] != null){
 $tick = file_get_contents('https://api.coinmarketcap.com/v1/ticker/' . $_POST['Crypto'] .'/?convert=' . $_POST['Fiat']); 
 $url = $tick;
 $data = json_decode($tick, TRUE);
 $cur = $data[0]["price_" . strtolower($_POST['Fiat'])]; //price_fiat
 $curDisplay = round($cur, 2);
-
+}
+else{ //default is bitcoin to usd
+    $tick = file_get_contents('https://api.coinmarketcap.com/v1/ticker/bitcoin/'); 
+    $url = $tick;
+    $data = json_decode($tick, TRUE);
+    $cur = $data[0]["price_usd"]; //price_fiat
+    $curDisplay = round($cur, 2);
+}
 ?>
     <script>
     function CryptoConvert(input){
@@ -48,14 +54,12 @@ $curDisplay = round($cur, 2);
         var co = document.getElementById('ci');
         ci.value = output;
     }
-
     function FiatConvert(input){
         var price = "<?php echo $curDisplay; ?>"; 
         var output = input.value / price;
         var co = document.getElementById('bi');
         bi.value = output;
     }
-
     </script>
 </div>
 
