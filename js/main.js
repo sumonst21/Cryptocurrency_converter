881d3a3ef71509f583a5664018f82714581c58c6
jens210 <jensvk94@gmail.com>
2018-01-09 10:39:24 +0100

    replace commas with dots

--- js/main.js
+++ js/main.js
@@ -38,3 +38,33 @@ function FiatConvert(input) {
     var co = document.getElementById('bi');
     bi.value = output;
 }
+
+// client-side validation of numeric inputs, optionally replacing separator sign(s).
+$("input.number").on("keydown", function (e) {
+    // allow function keys and decimal separators
+    if (
+        // backspace, delete, tab, escape, enter, comma and .
+        $.inArray(e.keyCode, [46, 8, 9, 27, 13, 108, 110, 188, 190]) !== -1 ||
+        // Ctrl/cmd+A, Ctrl/cmd+C, Ctrl/cmd+X
+        ($.inArray(e.keyCode, [65, 67, 88]) !== -1 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // home, end, left, right
+        (e.keyCode >= 35 && e.keyCode <= 39)) {
+        // optional: replace commas with dots in real-time (for en-US locals)
+        if (e.keyCode === 188 || e.keyCode === 108) {
+            e.preventDefault();
+            $(this).val($(this).val() + ".");
+        }
+/*
+     optional: replace dots with commas in real-time (for EU locals)
+        if (e.keyCode === 190) {
+            e.preventDefault();
+            $(this).val($(this).val() + ",");
+        }
+        */
+        return;
+    }
+    // block any non-number
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+    }
+});
